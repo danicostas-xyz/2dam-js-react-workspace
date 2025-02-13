ed7f3d5ca4be30c91f14306e6374f36396f03d55
Dani Costas <daniel.costas.rodriguez@bootcamp-upgrade.com>
2025-02-13 17:29:39 +0100

    Updated 03_react

--- 03_react/src/App.jsx
+++ 03_react/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './App.css'
 import Note from './pages/Note'
 import EmptyNote from './components/EmptyNote'
+import Modal from './components/Modal'
 
 export default function App () {
   // Aquí puedes hacer todo lo que quieras de js hasta que llegue el return, que es html
@@ -9,7 +10,7 @@ export default function App () {
   // Aquí empieza html
   return (
     <div className='app'>
-      <Note />
+      <Modal />
     </div>
   )
 }
--- 03_react/src/components/Modal.jsx
+++ 03_react/src/components/Modal.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
 import '../styles/Modal.css'
+import { FaInfoCircle } from 'react-icons/fa'
 
-export default function Modal(props) {
-    return ()
+export default function Modal (props) {
+  return (
+    <div className='modal'>
+
+      <FaInfoCircle className='modal__icon' />
+      <p className='modal__text'>Save changes</p>
+      <div className='modal__buttonWrapper'>
+        <button className='modal__button modal__button--danger'>Discard</button>
+        <button className='modal__button modal__button--success'>Save</button>
+      </div>
+    </div>
+  )
 }
